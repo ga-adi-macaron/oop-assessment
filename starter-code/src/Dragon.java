388316428a443b430d3dc7bc507ca0fe8d4d03be
Joe Lim <joelimyx@gmail.com>
2016-10-15 17:56:07 -0400

    Finsh Monster Project

--- starter-code/src/Dragon.java
+++ starter-code/src/Dragon.java
@@ -5,6 +5,18 @@
 //TODO: Make Dragon a subclass of Monster, implement the aboutMe method.
 //Example output for aboutMe: "I am a Dragon with 3 health and do 8 damage!"
 
-public class Dragon{
+public class Dragon extends Monster{
+    private int mNumberOfLegs;
+    private String mName;
 
+    public Dragon(String name, int damage, int health, int numberOfLegs) {
+        super(damage, health);
+        mNumberOfLegs = numberOfLegs;
+        mName = name;
+    }
+
+    @Override
+    public String aboutMe() {
+        return "I'm the legendary dragon, "+ mName + ", with " + super.getHealth() + " health and do "+super.getDamage()+" damage!";
+    }
 }
--- starter-code/src/Zombie.java
+++ starter-code/src/Zombie.java
@@ -5,6 +5,24 @@
 //TODO: Make Skeleton a subclass of monster, and implement the aboutMe method.
 //Example output of the aboutMe method: "I am a Zombie with 4 health and do 10 damage!"
 
-public class Zombie{
+public class Zombie extends Monster{
+    private int mNumOfLimbs;
 
+    public Zombie(int damage, int health) {
+        super(damage, health);
+        mNumOfLimbs = 3;
+    }
+
+    public int getNumOfLimbs() {
+        return mNumOfLimbs;
+    }
+
+    public void setNumOfLimbs(int numOfLimbs) {
+        mNumOfLimbs = numOfLimbs;
+    }
+
+    @Override
+    public String aboutMe() {
+        return "I am a Zombie with "+ getHealth() + "Health and does "+ getDamage() +" damage! But only have "+mNumOfLimbs+" limbs...";
+    }
 }
