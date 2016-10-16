2c8824ba005a37a64bf6b21194bf5964f6e83507
ECarrasco91 <ezequielcarrasco91@yahoo.com>
2016-10-16 11:29:17 -0400

    changes made

--- starter-code/src/Dragon.java
+++ starter-code/src/Dragon.java
@@ -5,6 +5,15 @@
 //TODO: Make Dragon a subclass of Monster, implement the aboutMe method.
 //Example output for aboutMe: "I am a Dragon with 3 health and do 8 damage!"
 
-public class Dragon{
+public class Dragon extends Monster{
 
+    public Dragon(int damage, int health) {
+        super(damage, health);
+    }
+
+    @Override
+    public void aboutMe() {
+        System.out.println("I am Dragon with " + getHealth() +
+                " health and do " + getDamage() + " damage!");
+    }
 }
--- starter-code/src/Zombie.java
+++ starter-code/src/Zombie.java
@@ -5,6 +5,15 @@
 //TODO: Make Skeleton a subclass of monster, and implement the aboutMe method.
 //Example output of the aboutMe method: "I am a Zombie with 4 health and do 10 damage!"
 
-public class Zombie{
+public class Zombie extends Monster{
 
+    public Zombie(int damage, int health) {
+        super(damage, health);
+    }
+
+    @Override
+    public void aboutMe() {
+        System.out.println("I am Zombie with " + getHealth() +
+                " health and do " + getDamage() + " damage!");
+    }
 }
