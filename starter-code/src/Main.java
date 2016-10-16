60a41d2d2ff8211fa84c90aab0cc86d2ef58953c
KorbKenny <korbkenny@gmail.com>
2016-10-16 18:38:49 -0400

    Finished hw

--- starter-code/src/Main.java
+++ starter-code/src/Main.java
@@ -6,8 +6,19 @@ public class Main {
     public static void main(String[] args) {
         //TODO: Create an array of Monsters, containing at least 2 different types (ie dragon and zombie)
 
+        Dragon jeff = new Dragon(12, 16);
+        Dragon roger = new Dragon(9, 19);
+        Dragon gertrude = new Dragon(11, 17);
+        Zombie rob = new Zombie(5, 3);
+
+        Monster[] monsters = {jeff, roger, gertrude, rob};
 
         //TODO: Loop through the array of Monsters, printing out something for each Monster
+
+        for (Monster monster : monsters) {
+            System.out.println(monster.aboutMe());
+        }
+
 
     }
 }
